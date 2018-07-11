56a6bf430083f995e535ce2fac76e596ad9306e0
kusenko <kusenko@atlassian.com>
2018-07-11 18:14:18 +0300

    extract IOpenFileDialog

---
+++ NUnit3Gui/Interfaces/IOpenFileDialog.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Forms;
+
+namespace NUnit3Gui.Interfaces
+{
+    public interface IOpenFileDialog
+    {
+        string FileName { get; set; }
+
+        string Filter { get; set; }
+
+        bool Multiselect { get; set; }
+
+        DialogResult ShowDialog();
+
+        string[] FileNames { get; }
+    }
+}
