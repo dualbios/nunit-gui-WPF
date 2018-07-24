49ad7d061ac4d33892cc6bfd3f9db138613a2ccf
kusenko <kusenko@atlassian.com>
2018-07-24 12:23:12 +0300

    added NUnitParser

--- NUnit3Gui/Instanses/FileParsers/FileParserManager.cs
+++ NUnit3Gui/Instanses/FileParsers/FileParserManager.cs
@@ -11,8 +11,9 @@ namespace NUnit3Gui.Instanses.FileParsers
     [Export(typeof(IFileParserManager))]
     public class FileParserManager : IFileParserManager
     {
+        private readonly string currentParserAlias = "NUnitParser";//"CmdParser"
 
-        public IFileParser CurrentFileParser => FileParsers.First(_ => _.Alias == "CmdParser");
+        public IFileParser CurrentFileParser => FileParsers.First(_ => _.Alias == currentParserAlias);
 
         [ImportMany]
         public IEnumerable<IFileParser> FileParsers { get; private set; }
---
+++ NUnit3Gui/Instanses/FileParsers/NUnitFileParser.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using NUnit.Engine;
+using NUnit3Gui.Interfaces;
+
+namespace NUnit3Gui.Instanses.FileParsers
+{
+    [Export(typeof(IFileParser))]
+    public class NUnitFileParser : IFileParser
+    {
+        private ITestEngine _testEngine;
+
+        public NUnitFileParser()
+        {
+            _testEngine = TestEngineActivator.CreateInstance(true);
+        }
+
+        public string Alias => "NUnitParser";
+
+        public string Name => "NUnit parser";
+
+        public Task<IEnumerable<ITest>> ParseFileAsync(string fileName, CancellationToken ct)
+        {
+            var _package = MakeTestPackage(fileName);
+            ITestRunner Runner = _testEngine.GetRunner(_package);
+            System.Xml.XmlNode result = Runner.Explore(TestFilter.Empty);
+
+            var resultExplore = GetAttribute(result, "result");
+            if (resultExplore == "Failed")
+                throw new Exception($"Loading failed. {resultExplore}");
+
+            return Task.FromResult(Enumerable.Empty<ITest>());
+        }
+
+        private TestPackage MakeTestPackage(string fileName)
+        {
+            var package = new TestPackage(fileName);
+
+            return package;
+        }
+
+        public static string GetAttribute(XmlNode result, string name)
+        {
+            XmlAttribute attr = result.Attributes[name];
+
+            return attr == null ? null : attr.Value;
+        }
+    }
+}
