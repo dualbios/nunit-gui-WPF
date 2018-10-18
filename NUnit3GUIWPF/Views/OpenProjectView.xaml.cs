13f6bece78f936ca1f7d2b42c3f4fb030f92acfe
Bios-Laptop\Bios <usenkoko@ukr.net>
2018-10-18 22:39:40 +0300

    implement multiple-file projects

--- NUnit3GUIWPF/Views/OpenProjectView.xaml.cs
+++ NUnit3GUIWPF/Views/OpenProjectView.xaml.cs
@@ -1,18 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Microsoft.Win32;
+﻿using System.Windows.Controls;
 
 namespace NUnit3GUIWPF.Views
 {
@@ -24,20 +10,6 @@ namespace NUnit3GUIWPF.Views
         public OpenProjectView()
         {
             InitializeComponent();
-        }
-
-        private void SelectFile_OnClick(object sender, RoutedEventArgs e)
-        {
-            OpenFileDialog ofd = new OpenFileDialog()
-            {
-                CheckFileExists = true,
-                DefaultExt = "*.dll"
-            };
-            string file = (ofd.ShowDialog(Application.Current.MainWindow) == true) ? ofd.FileName : null;
-            if (file != null)
-            {
-                FilePathTextBox.Text = file;
-            }
         }
     }
 }
