49e8d5f2c8a34b8843be0a10fc4c1e4c4e967394
kusenko <kusenko@atlassian.com>
2018-11-09 12:20:18 +0200

    update build process

--- NUnit3GUIWPF/build package.cmd
+++ NUnit3GUIWPF/build package.cmd
@@ -1,1 +1,3 @@
-nuget pack -build -Properties Configuration=release
+set arg1=%1
+nuget pack -build -Properties Configuration=%arg1%
+start /d "bin\"%arg1% "ss" "%programfiles%\7-zip\7z.exe" a ..\..\NUnit3GUIWPF.zip *.*
